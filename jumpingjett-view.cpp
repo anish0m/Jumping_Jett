28690923d78a1be1771dfd469afcb9806ee0100a
Anishom <mohsina.tabassum.rifa@gmail.com>
2020-12-23 17:09:39 +0600

    Poster add

--- jumpingjett-view.cpp
+++ jumpingjett-view.cpp
@@ -9,3 +9,9 @@ void drawBackgroundImage(SDL_Renderer* renderer, SDL_Surface *backgroundImg, SDL
 }
 
 // RIFA: Implement "drawJettPoster" as same way as above with "Jett.bmp" image file
+void drawJettPoster(SDL_Renderer* renderer, SDL_Surface *jettImg, SDL_Texture *texture) {
+    SDL_Rect jettImageBoundaries = {JETT_X, JETT_Y, JETT_WIDTH, JETT_HEIGHT };
+    jettImg = SDL_LoadBMP("../Jett.bmp");
+    texture = SDL_CreateTextureFromSurface(renderer, jettImg);
+    SDL_RenderCopy(renderer, texture, NULL, &jettImageBoundaries);
+}
--- jumpingjett-view.h
+++ jumpingjett-view.h
@@ -1,5 +1,3 @@
-
-
 #ifndef JJGAME_JUMPINGJETT_VIEW_H
 #define JJGAME_JUMPINGJETT_VIEW_H
 
@@ -11,9 +9,15 @@
 #define GAME_VIEW_HEIGHT SCREEN_HEIGHT - GAP - GAP
 
 // RIFA: define Jett poster's dimensions (width 170, height 200) as above
+#define JETT_WIDTH      170
+#define JETT_HEIGHT     200
+
+#define JETT_X      (GAP * 3 + GAME_VIEW_WIDTH)
+#define JETT_Y      (GAP * 4)
 
 void drawBackgroundImage(SDL_Renderer* renderer, SDL_Surface *backgroundImg, SDL_Texture *texture);
 
 // RIFA: create a function prototype "drawJettPoster" like the above
+void drawJettPoster(SDL_Renderer* renderer, SDL_Surface *jettImg, SDL_Texture *texture);
 
 #endif //JJGAME_JUMPINGJETT_VIEW_H
